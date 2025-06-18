10fdf5f093821bf42a8a8b2ba76bcbb6cbdc5d44
gogood-fb <frank.biasi@gogoodtravel.org>
2025-06-18 16:56:30 -0600

    Add comprehensive .gitignore and clean up tracked files that should be ignored

--- composeApp/build/generated/ksp/iosSimulatorArm64/iosSimulatorArm64Main/kotlin/org/gogood/data/database/GoGoodDatabaseConstructor.kt
+++
@@ -1,8 +0,0 @@
-package org.gogood.`data`.database
-
-import androidx.room.RoomDatabaseConstructor
-
-public actual object GoGoodDatabaseConstructor : RoomDatabaseConstructor<GoGoodDatabase> {
-  actual override fun initialize(): GoGoodDatabase =
-      org.gogood.`data`.database.GoGoodDatabase_Impl()
-}
